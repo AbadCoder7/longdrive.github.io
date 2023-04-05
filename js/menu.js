7d0bf3835ec9f466b62b314028fb2046bcc03540
LampGuy <129078180+AbadCoder7@users.noreply.github.com>
2023-04-05 13:53:57 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -7,29 +7,19 @@ function mainMenuNav(x) {
       document.getElementById("main").style.display = "none";
       document.getElementById("death").style.display = "none";
       document.getElementById("about").style.display = "none";
-      document.getElementById("achi").style.display = "none";
       document.getElementById("change").style.display = "none";
-      setTimeout(randomEvent, 20000);
+      setTimeout(randomEvent, 5000);
    }
    else if (x == 2) {
       document.getElementById("main").style.display = "none";
       document.getElementById("death").style.display = "none";
-      document.getElementById("achi").style.display = "none";
       document.getElementById("change").style.display = "none";
       document.getElementById("about").style.display = "block";
-   }
-   else if (x == 3) {
-      document.getElementById("achi").style.display = "block";
-      document.getElementById("main").style.display = "none";
-      document.getElementById("death").style.display = "none";
-      document.getElementById("about").style.display = "none";
-      document.getElementById("change").style.display = "none";
    }
    else if (x == 4 && gamerunning == false) {
       document.getElementById("main").style.display = "none";
       document.getElementById("death").style.display = "none";
       document.getElementById("about").style.display = "none";
-      document.getElementById("achi").style.display = "none";
       document.getElementById("change").style.display = "block";
    }
    else if (x == 99) {
@@ -41,11 +31,18 @@ function mainMenuNav(x) {
       document.getElementById("main").style.display = "block";
       document.getElementById("death").style.display = "none";
       document.getElementById("about").style.display = "none";
-      document.getElementById("achi").style.display = "none";
       document.getElementById("change").style.display = "none";
    }
 }
 document.getElementById("change").style.display = "none";
+
+
+var score = 0
+var highestscore = 0
+
+// score enhancers
+var corpses = 0
+var genocide = false
 
 function die() {
    if (fuel <= 0 || stat <= 0 || nutr <= 0) {
@@ -54,17 +51,33 @@ function die() {
       else if (stat <= 0) {document.getElementById("whyusuck").innerHTML = "Your car broke down";}
       else if (nutr <= 0) {document.getElementById("whyusuck").innerHTML = "You starved to death";}
       
+
       eventgoing = true;
       statstop = true;
       gamerunning = false;
       evpop = "";
       document.getElementById("death").style.display = "block";
-      score = miles;
+      
+      // score handler
+      var scoreadded = 0
+      
+      scoreadded = scoreadded + miles * 10;
+      scoreadded = scoreadded + corpses * 50;
+      
+      if (genocide == true) {scoreadded = scoreadded + 10000;}
+      
+      score = scoreadded;
       document.getElementById("dscore").innerHTML = score;
-      if (score > bestscore) {
-         bestscore = score;
-         //document.getElementById("bestscore").innerHTML = bestscore;
+      if (score > highestscore) {
+         highestscore = score;
+         document.getElementById("highestscore").innerHTML = highestscore;
       }
+      
+      // clearing stuff
+      corpses = 0;
+      scoreadded = 0;
+      genocide = false;
+      
       document.getElementById("event").style.display = "none";
       document.getElementById("shop").style.display = "none";
    }
@@ -74,10 +87,6 @@ function mainBack() {
    document.getElementById("main").style.display = "block";
    document.getElementById("death").style.display = "none";
    document.getElementById("about").style.display = "none";
-   document.getElementById("achi").style.display = "none";
    document.getElementById("change").style.display = "none";
 }
 document.getElementById("about").style.display = "none";
-
-var score = 0
-var bestscore = 0
