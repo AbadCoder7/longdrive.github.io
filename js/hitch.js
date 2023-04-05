f835c6e662a52cff3f18a079ac6958c758f338fe
LampGuy <129078180+AbadCoder7@users.noreply.github.com>
2023-04-05 13:53:23 -0500

    Update hitch.js

--- js/hitch.js
+++ js/hitch.js
@@ -32,6 +32,73 @@ function deerMsg(x) {
    nutr + 5;
 }
 
+function familyMsg(x, y) {
+   if (x != y) {
+      evpop = "<p class='evmes'>They're all dead :D</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Family Crossing";
+      document.getElementById("evopts").innerHTML = evpop;
+      nutr = 1000;
+      // You know why this is here
+   }
+   else if (x == y) {
+      evpop = '<p class="evmes">Your hood has been damaged by your actions</p><p class="evopt" onclick="contRoad()">Continue on the road</p>';
+      document.getElementById("evn").innerHTML = "Family Crossing";
+      document.getElementById("evopts").innerHTML = evpop;
+      stat = stat - 100;
+   }
+}
+
+function mysteryMsg(x) {
+   if (x == 1) {
+      var moneyadded = 0
+      moneyadded = Math.floor(Math.random() * 130) + 20;
+      money = money + moneyadded;
+      
+      evpop = "<p class='evmes'>It contained $"+moneyadded+"!</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Mysterious Item";
+      document.getElementById("evopts").innerHTML = evpop;
+   }
+   else if (x == 2) {
+      evpop = "<p class='evmes'>It contained a can of beans!</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Mysterious Item";
+      document.getElementById("evopts").innerHTML = evpop;
+      
+      nutr = nutr + 100;
+   }
+   else if (x == 3) {
+      evpop = "<p class='evmes'>Whoops, it was an explosive!</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Mysterious Item";
+      document.getElementById("evopts").innerHTML = evpop;
+      
+      stat = stat - 300;
+   }
+   else if (x == 3) {
+      evpop = "<p class='evmes'>It contained a gasoline can!</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Mysterious Item";
+      document.getElementById("evopts").innerHTML = evpop;
+      
+      stat = stat - 300;
+   }
+}
+
+function hurtmanMsg(x) {
+   if (x == 1) {
+      var moneyadded = 0
+      moneyadded = Math.floor(Math.random() * 30) + 20;
+      money = money + moneyadded;
+      
+      evpop = "<p class='evmes'>You borrowed $"+moneyadded+"</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Injured Man";
+      document.getElementById("evopts").innerHTML = evpop;
+   }
+   else if (x == 2) {
+      evpop = "<p class='evmes'>The road becomes ever more red</p><p class='evopt' onclick='contRoad()'>Continue on the road</p>";
+      document.getElementById("evn").innerHTML = "Injured Man";
+      document.getElementById("evopts").innerHTML = evpop;
+      
+      nutr = nutr + 50;
+   }
+}
 
 var hit1 = function() {
    statstop = true;
